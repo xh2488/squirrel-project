f0b66c24257a5b7bf322137310b084de3f302722
hc3255 <80859538+hc3255@users.noreply.github.com>
2021-04-12 21:46:10 -0400

    view4

--- squirrel_app/views.py
+++ squirrel_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from squirrel_app.models import squirrel_info
+from squirrel_app.models import AddSquirrelForm
 from django.shortcuts import get_object_or_404
 import pandas as pd
 
@@ -24,7 +25,20 @@ def sighting_individual(request,squirrel_id):
     print(squirrel)
     return render(request, 'squirrel_app/sighting_individual.html', {"squirrel": squirrel})
 
+def add_squirrel(request):
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = AddSquirrelForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            form.save()
+            print("new squirrel added")
+            return HttpResponse("Thank you! Squirrel Added!")
 
+    else:
+        form = AddSquirrelForm()
+
+    return render(request, 'squirrel_app/add.html', {'form': form})
 
 
 
